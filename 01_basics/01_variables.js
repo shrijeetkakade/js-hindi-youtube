f7dccc94a3fb8d761b1b33f2b929f2a3e28cad81
shrijeetkakade <shrijeetkakade565@gmail.com>
2026-02-10 12:33:32 +0000

    changes 2 pushed

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,2 +1,18 @@
 let name = 'shrijeet';
 console.log(name);
+/* 
+don't use var use let instead
+because of issue in block scope and functional scope 
+*/
+// Account details using different data types
+
+const accountName = "Shrijeet";        // const → value should not change
+let accountPassword = "pass@123";      // let → value can be updated
+var accountCity = "Mumbai";             // var → function/global scoped (not recommended)
+
+// Display data in table format
+console.table({
+  AccountName: accountName,
+  AccountPassword: accountPassword,
+  AccountCity: accountCity
+});
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict";// treat all JS code as a newer version
+
+// alert(3+3) // we are using node js not browser there is a different way for using alert in js
+
+console.log(3+2); console.log("shrijeet")
+/*
+above console log syntax is correct but this practice is not good by doing 
+this we are damaging the readability of code 
+code redable hona chahiye
+so don't prefer  this practices
+*/
+
+// code readability should be high
+
+let name = "shrijeet"
+let age = 19
+let isLoggedIn = false
+let state;
+
+// number -> range is (2 raised to 53) -1
+// else if no. is bigger we have bigint named variable
+// string can be in ' ' or " "
+// boolean == value canbe either true or false
+// null is actually an standalone value
+// undefined is something you have just given a name and didn't defined its value declared variable but not given value
+// symbol 
+
+
+
+// object
+
+console.log(typeof false );
