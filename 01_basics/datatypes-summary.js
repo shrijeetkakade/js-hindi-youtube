859227efe693e87fc941c2781f7fdaa36a2d5488
shrijeetkakade <shrijeetkakade565@gmail.com>
2026-02-14 04:06:02 +0000

    theory pushed

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,3 +1,11 @@
 // there are two types of datatypes primitive and non-primitive
 // differentiation is done by how the data is stored in memory and how can you access it
 //#primitive
+// 7 types = string , boolean , number , null , undefined , symbol , BigInt
+
+//#reference (Non-Primitive data type)
+// Array , objects , functions
+
+// JavaScript is a dynamically typed language.
+
+console.log(100.3 * 432.3);
