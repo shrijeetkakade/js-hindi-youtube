b98d68c1b5b51b871d788424c1937b06c91905ce
shrijeetkakade <shrijeetkakade565@gmail.com>
2026-02-22 07:11:50 +0000

    array dec

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,19 @@
+// Array
+// array is made up of elements
+// javascript arrays have 0 based indexing  
+// arrays use shallow copy matlab jo bhi change karenge wo orignal 
+// array mein bhi hoga ......... 
+// there are another type of copies that is deep copies
+// deep copy do not share the same reference
+
+const myArr = [0,1,2,3,4,5,true,"hitesh"]
+
+const myHeroes = ["shaktiman","spidermaan","rocky-bhai","oggy","jack"]
+
+console.log(myArr[3]);
+
+// there is one more way for declaring the array
+const myarr2 = new Array(1,2,3,4)
+
+console.log(myarr2[3]);
+// array methods ......................................
