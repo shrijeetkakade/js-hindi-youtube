89e025a89e02efe7f9c2b2eab8ca3bf401476640
shrijeetkakade <shrijeetkakade565@gmail.com>
2026-02-27 05:29:49 +0000

    functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,44 @@
+function sayMyName(){
+console.log('s');
+console.log('h');
+console.log('r');
+console.log('i');
+console.log('j');
+console.log('e'); 
+console.log('e');
+console.log('t');
+}
+// sayMyName();
+
+// function addTwoNumbers(a, b) { // function ki definition banate waqt jo bhi input lete hain usey hum parameters bolte hain 
+//     console.log(a + b);
+// }
+function addTwoNumbers(a, b) { // function ki definition banate waqt jo bhi input lete hain usey hum parameters bolte hain 
+   let result;
+   result = a + b;
+   return result;
+}
+const result = addTwoNumbers(5,9); // jab functionn call karte hain tab use parameters nahi arguments kehete hain
+
+// console.log(result);
+
+function loginusermessage(username){
+    if(username === undefined){
+     console.log("please enter a username");
+     return 0;
+     
+    }
+    return `${username} just logged in`
+}
+
+const whathappen = loginusermessage("shrijeet");
+// console.log(whathappen) 
+
+
+// function with object
+ function calculatecartprice(...num1 ){
+   return num1
+ }
+
+ console.log(calculatecartprice(200,400,500));
+ 
