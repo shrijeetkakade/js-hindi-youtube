5162e086434defccbfa40f202d2b79a35b272c0f
shrijeetkakade <shrijeetkakade565@gmail.com>
2026-03-02 12:56:59 +0000

    truthy pushed

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,7 @@
+const  userEmail = "h@hitesh.ai"
+// here in condition of if statement 
+if(userEmail){
+    console.log(`got user email ${userEmail}`);
+}else{
+    console.log("don't have user email");
+    }
